2a41fafa9ff80fe667139b0e200cd1c480f61eea
gauryn <gauryn22@gmail.com>
2016-03-21 20:50:19 -0400

    Changed dropdown for gender and age to radio buttons

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,13 +1,2 @@
 module ItemsHelper
-
-	# get gender list for index view of items page
-	def get_gender_list
-		Item::GENDER_LIST
-	end
-
-	# get age list for index view of items page
-	def get_age_list
-	 	Item::AGE_LIST 
-	end 
-
 end
