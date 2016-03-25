a423c8e8cb60ef537bdd810f9466167f9d43a642
gauryn <gauryn22@gmail.com>
2016-03-25 09:31:49 -0400

    Created development db in postgres, added username to config database.yml file, removed .sqlite3 in db names in database.yml file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318163456) do
+ActiveRecord::Schema.define(version: 20160325051341) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "basket_items", force: :cascade do |t|
     t.integer  "quantity"
@@ -37,13 +40,13 @@ ActiveRecord::Schema.define(version: 20160318163456) do
     t.string   "barcode"
     t.integer  "quantity"
     t.string   "name"
-    t.integer  "gender"
     t.integer  "age"
     t.text     "notes"
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.boolean  "donated"
+    t.integer  "gender"
   end
 
 end
