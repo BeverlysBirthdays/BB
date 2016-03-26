f3bd63bf4ad82b70973146c47266ab2962b60a01
gauryn <gauryn22@gmail.com>
2016-03-26 15:12:36 -0400

    Added icon attribute to category model

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160325051341) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.string   "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
