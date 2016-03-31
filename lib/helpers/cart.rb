ce1e135fab6d916b7516a09d41146a46ed4a5339
gauryn <gauryn22@gmail.com>
2016-03-31 09:15:52 -0400

    Can have incremental quantities to items in bin

--- lib/helpers/cart.rb
+++ lib/helpers/cart.rb
@@ -18,18 +18,18 @@ module BbInventoryHelpers
     end
 
     def add_item_to_cart(item_id, qty=1)
-      if session[:cart].keys.include?(item_id)
+      if session[:cart].keys.include?(item_id.to_s)
         # if item in cart, increment quantity by 1
-        session[:cart][item_id] += qty
+        session[:cart][item_id.to_s] += qty
       else
         # add it to the cart
-        session[:cart][item_id] = qty
+        session[:cart][item_id.to_s] = qty
       end
     end
 
     def remove_item_from_cart(item_id)
-      if session[:cart].keys.include?(item_id)
-        session[:cart].delete(item_id)
+      if session[:cart].keys.include?(item_id.to_s)
+        session[:cart].delete(item_id.to_s)
       end
     end
 
