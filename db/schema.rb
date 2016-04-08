98603cc97e3d8d501c485c10079cfe663d52f395
mogh <mogh@example.com>
2016-04-08 18:09:24 -0400

    generated programs model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407014303) do
+ActiveRecord::Schema.define(version: 20160408220745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,5 +60,11 @@ ActiveRecord::Schema.define(version: 20160407014303) do
   end
 
   add_index "items", ["age"], name: "index_items_on_age", using: :gin
+
+  create_table "programs", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
