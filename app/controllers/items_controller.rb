5a9087a45092249ebc9901ccb9f2fcc3d07dc12f
gauryn <gauryn22@gmail.com>
2016-04-08 22:20:19 -0400

    Added price to show and index pages

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -122,6 +122,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:barcode, :quantity, :name, :gender, :donated, :notes, :category_id, :age=>[])
+      params.require(:item).permit(:barcode, :unit_price, :quantity, :name, :gender, :donated, :notes, :category_id, :age=>[])
     end
 end
