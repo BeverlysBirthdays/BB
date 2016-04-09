8b226bb17393618c5c08059c4492b073272edfb6
mogh <mogh@example.com>
2016-04-08 20:02:36 -0400

    crud for programs

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -9,28 +9,50 @@ class ProgramsController < ApplicationController
   end
 
   def new
+    @program = Program.new
   end
 
   def create
-  end
+    @program = Program.new(program_params)
 
- 
+    respond_to do |format|
+      if @program.save
+        format.html { redirect_to @program, notice: 'Program was successfully created.' }
+        format.json { render :show, status: :created, location: @program }
+      else
+        format.html { render :new }
+        format.json { render json: @program.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   def edit
   end
 
-  
-
   def update
+    respond_to do |format|
+      if @program.update(program_params)
+        format.html { redirect_to @program, notice: 'Program was successfully updated.' }
+        format.json { render :show, status: :ok, location: @program }
+      else
+        format.html { render :edit }
+        format.json { render json: @program.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def delete
+  def destroy
+    @program.destroy
+    respond_to do |format|
+      format.html { redirect_to programs_url, notice: 'Program was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
   
   private 
     # Use callbacks to share common setup or constraints between actions.
     def set_program
-      @program= Item.find(params[:id])
+      @program = Program.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
