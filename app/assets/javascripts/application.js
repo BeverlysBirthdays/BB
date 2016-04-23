f32c59868d2e74eea090ae5d7b78b812b8254c8f
gauryn <gauryn22@gmail.com>
2016-04-23 15:15:08 -0500

    Added ability to edit user credentials. Dropdown for user not working in top nav. Only works in side nav.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,9 +29,14 @@ $(document).ready(function() {
 });
 
 $(".button-collapse").sideNav();
-$( document ).ready(function() { 
-	$(".dropdown-button").dropdown(); 
-});
+
+$('.dropdown-button').dropdown({
+  inDuration: 300,
+  outDuration: 225,
+  hover: true, // Activate on hover
+  belowOrigin: true // Displays dropdown below the button
+}
+);
 
 $(document).ready(function(){
     $('.tooltipped').tooltip({delay: 50});
