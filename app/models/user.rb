d6fb10b3668e50fa39d56b11f32c29eefb888e32
gauryn <gauryn22@gmail.com>
2016-04-23 12:15:29 -0500

    Added methods and validaiton to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ActiveRecord::Base
+
+	# Use built-in rails support for password protection
+	has_secure_password
+
+	# Relationships
+
+	# Validations
+	validates_uniqueness_of :username
+	validates_presence_of :password, on: :create 
+	validates_presence_of :password_confirmation, on: :create 
+	validates_confirmation_of :password, message: "does not match"
+	validates_length_of :password, minimum: 4, message: "must be at least 4 characters long"
+
+	# Scopes
+
+	# Methods
+
+	# login by email address
+	def self.authenticate(email, password)
+		find_by_username(username).try(:authenticate, password)
+	end
+
 end
