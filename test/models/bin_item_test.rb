9f141fd831a292715769dfcb483eb0fb22e5c805
mogh <mogh@example.com>
2016-04-23 11:34:45 -0400

    bin_item validation tests

--- test/models/bin_item_test.rb
+++ test/models/bin_item_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class BinItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # testing relationships
+  should belong_to(:bin)
+  should belong_to(:item_checkin)
+  should belong_to(:item_checkin_archive)
+
+  # testing validations
+  should validate_numericality_of(:quantity)
+  should allow_value(5).for(:quantity)
+  should_not allow_value(0).for(:quantity)
+  should_not allow_value(-3).for(:quantity)
+  should_not allow_value(3.14159).for(:quantity)
+  should_not allow_value("bad").for(:quantity)
 end
