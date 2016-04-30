cd2b8bf7331b4740d20c5a2bff7f1925efa79030
gauryn <gauryn22@gmail.com>
2016-04-30 09:56:39 -0400

    Added partials with summary buttons about total items, donated, bins and agencies.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,10 @@ class HomeController < ApplicationController
 	def home
 		@items_by_category = get_category_count
 		@bins_by_agency = Bin.by_agency_and_program
+		@items = Item.all.alphabetical
+		@donated_items_count = calculate_total_donated_quantity(@items)
+		@bins = Bin.all.chronological
+		@agencies = Agency.all.alphabetical
 	end
 
 	def about_us
